5801029c1f9cb11482a8f4229cc564d93a3e2d94
Janek Spaderna <janek@spaderna.eu>
2023-09-14 17:47:17 +0200

    Simple program using graphviz lib

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Foreign
+import Foreign.C
+
+data GVContext
+
+foreign import ccall gvContext :: IO (Ptr GVContext)
+
+foreign import ccall gvcVersion :: Ptr GVContext -> IO CString
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  c <- gvContext
+  putStrLn $ "context lives at: " ++ show c
+  v <- peekCString =<< gvcVersion c
+  putStrLn $ "context version: " ++ v
